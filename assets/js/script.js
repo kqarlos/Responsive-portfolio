cb108873e0ffef4f2b02a8c4cac41c4000ace078
kqarlos <kqarlos@hotmail.com>
2020-01-06 21:42:51 -0800

    Updates styling for 'mywork' and 'top' buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,32 +21,6 @@ $(document).ready(function () {
             });
         } // End if
     });
-
-    // Add smooth scrolling to all links
-    $("myBtn").on('click', function (event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
-
-
-
 });
 
 $("#sendMessage").on("click", function (event) {
@@ -60,8 +34,8 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        $("#myBtn").css("display", "block");
+        $("#topBtn").css("display", "block");
     } else {
-        $("#myBtn").css("display", "none");
+        $("#topBtn").css("display", "none");
     }
 }
