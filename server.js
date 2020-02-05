2501a119cb294fc4a3829305f75ebf49b136cf54
kqarlos <kqarlos@hotmail.com>
2020-02-05 07:20:29 -0800

    Sets up server for portfolio

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var db = require("./models");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var handlebars = require("express-handlebars");
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var db = require("./models");
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+
+db.sequelize.sync().then(function () {
+
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
