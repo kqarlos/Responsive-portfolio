31fdd018ea50d745584c78ffed0f332559d3c596
kqarlos <kqarlos@hotmail.com>
2020-02-05 09:56:45 -0800

    Uses handlebars to display html to client

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = function (app) {
 
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname + "/public/index.html"));
+        res.render("index");
     });
 
 };
