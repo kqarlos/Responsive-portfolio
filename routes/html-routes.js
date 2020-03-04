e705e451e959f7c43f26c7baaac5f9cfeb9119e2
kqarlos <kqarlos@hotmail.com>
2020-03-04 13:08:16 -0800

    project partial displays project specific information

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,22 +3,26 @@ const path = require("path");
 module.exports = function (app) {
 
     app.get("/", (req, res) => {
-        res.render("index", { projectDisplay: 'none', aboutDisplay: 'block' });
+        res.render("index", {
+            projectDisplay: 'none',
+            aboutDisplay: 'block',
+            portfolioDisplay: 'none'
+        });
     });
 
     app.get("/:project", (req, res) => {
-        // res.render("project");
-        console.log(req.params.project);
         switch (req.params.project) {
             case 'artiste':
                 res.render('index', {
                     projectDisplay: 'block',
+                    portfolioDisplay: 'block',
                     aboutDisplay: 'none',
                     name: "Artiste Affame",
                     description: "Full-stack MERN application allows users to immerse themselves in the life of a renaissance artist. Users can play games, draw and share their art.",
                     stack: ["MongoDB", "Express", "React", "Node", "Mongoose", "Materialize", "Axios"],
                     repo: "https://github.com/kqarlos/artiste-affame",
-                    deployed: "https://artiste-affame.herokuapp.com/"
+                    deployed: "https://artiste-affame.herokuapp.com/",
+                    src: ""
                 });
                 break;
             case 'electionYear':
@@ -29,7 +33,8 @@ module.exports = function (app) {
                     description: "Full-stack application that allows to create an account and search for their representatives.",
                     stack: ["MySQL", "Express", "Node", "Handlebars", "Sequelize", "Bootstrap", "Charts.js", "Axios"],
                     repo: "https://github.com/kqarlos/election-year",
-                    deployed: "https://electionyear2020.herokuapp.com/"
+                    deployed: "https://electionyear2020.herokuapp.com/",
+                    src: "assets/images/election-year.png"
                 });
                 break;
             case 'memoryGame':
@@ -40,7 +45,8 @@ module.exports = function (app) {
                     description: "Choose a game theme and start playing. Only click each image once. Click twice and it's game over",
                     stack: ["React", "Node", "Bootstrap", "Axios"],
                     repo: "https://github.com/kqarlos/memory-game",
-                    deployed: "https://kqarlos.github.io/memory-game/"
+                    deployed: "https://kqarlos.github.io/memory-game/",
+                    src: "assets/images/memory-game.png"
                 });
                 break;
             case 'noteTaker':
@@ -51,7 +57,8 @@ module.exports = function (app) {
                     description: "Real-time note taking application. Take notes, update them, and delete them with this note-taker.",
                     stack: ["Node", "Express", "Bootstrap"],
                     repo: "https://github.com/kqarlos/note-taker",
-                    deployed: "https://note-taker2020.herokuapp.com/"
+                    deployed: "https://note-taker2020.herokuapp.com/",
+                    src: "assets/images/note-taker.png"
                 });
                 break;
             case 'googleLibrary':
@@ -62,7 +69,8 @@ module.exports = function (app) {
                     description: "Search through Google Books API and keep a list of your favorite books.",
                     stack: ["MongoDB", "Express", "React", "Node", "Bootstrap", "Axios", "Mongoose"],
                     repo: "https://github.com/kqarlos/google-library",
-                    deployed: "https://google-library-mern.herokuapp.com/"
+                    deployed: "https://google-library-mern.herokuapp.com/",
+                    src: "assets/images/google-library.png"
                 });
                 break;
             case 'passwordGenerator':
@@ -73,7 +81,8 @@ module.exports = function (app) {
                     description: "Generates a random password based on user preferences and copies it to the clipboard.",
                     stack: ["HTML", "CSS", "Javascript", "Bootstrap"],
                     repo: "https://github.com/kqarlos/password-generator",
-                    deployed: "https://kqarlos.github.io/password-generator/"
+                    deployed: "https://kqarlos.github.io/password-generator/",
+                    src: "assets/images/password-generator.png"
                 });
                 break;
             case 'dayPlanner':
@@ -84,7 +93,8 @@ module.exports = function (app) {
                     description: "A planner for day activities that saves information to local storage",
                     stack: ["HTML", "CSS", "Javascript", "Moment.js", "Bootstrap"],
                     repo: "https://github.com/kqarlos/day-planner",
-                    deployed: "https://kqarlos.github.io/day-planner/"
+                    deployed: "https://kqarlos.github.io/day-planner/",
+                    src: "assets/images/day-planner.png"
                 });
                 break;
             case 'weatherDashboard':
@@ -95,7 +105,8 @@ module.exports = function (app) {
                     description: "Displays current weather and forecast for a searched city.",
                     stack: ["HTML", "CSS", "Javascript", "Moment.js", "Bootstrap"],
                     repo: "https://github.com/kqarlos/weather-dashboard",
-                    deployed: "https://kqarlos.github.io/weather-dashboard/"
+                    deployed: "https://kqarlos.github.io/weather-dashboard/",
+                    src: "assets/images/weather-dashboard.png"
                 });
                 break;
             case 'dinnerParty':
@@ -106,7 +117,8 @@ module.exports = function (app) {
                     description: "Application that recommends a dish and a cocktail based on a protein choice",
                     stack: ["HTML", "CSS", "Javascript", "Materialize", "Bootstrap"],
                     repo: "https://github.com/kqarlos/dinner-party",
-                    deployed: "https://kqarlos.github.io/dinner-party/"
+                    deployed: "https://kqarlos.github.io/dinner-party/",
+                    src: "assets/images/dinner-party.png"
                 });
                 break;
             case 'shoppingBuddy':
@@ -117,7 +129,8 @@ module.exports = function (app) {
                     description: "Manage your shopping list on the go",
                     stack: ["HTML", "CSS", "Javascript", "Express", "Node", "Handlebars", "MySQL"],
                     repo: "https://github.com/kqarlos/shopping-buddy",
-                    deployed: "https://shopping-buddy2020.herokuapp.com/"
+                    deployed: "https://shopping-buddy2020.herokuapp.com/",
+                    src: "assets/images/shopping-buddy.png"
                 });
                 break;
             default:
