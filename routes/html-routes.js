5098fce630e93f45012317c8dae96bf3b505b1f9
kqarlos <kqarlos@hotmail.com>
2020-03-04 11:26:03 -0800

    Updates more detailed project specific information to be rendered

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,13 +10,25 @@ module.exports = function (app) {
         // res.render("project");
         console.log(req.params.project);
         switch (req.params.project) {
+            case 'artiste':
+                res.render('index', {
+                    projectDisplay: 'block',
+                    aboutDisplay: 'none',
+                    name: "Artiste Affame",
+                    description: "Full-stack MERN application allows users to immerse themselves in the life of a renaissance artist. Users can play games, draw and share their art.",
+                    stack: ["MongoDB", "Express", "React", "Node", "Mongoose", "Materialize", "Axios"],
+                    repo: "https://github.com/kqarlos/artiste-affame",
+                    deployed: "https://artiste-affame.herokuapp.com/"
+                });
+                break;
             case 'electionYear':
                 res.render('index', {
                     projectDisplay: 'block',
                     aboutDisplay: 'none',
                     name: "Election Year",
-                    description: "",
-                    repo: "",
+                    description: "Full-stack application that allows to create an account and search for their representatives.",
+                    stack: ["MySQL", "Express", "Node", "Handlebars", "Sequelize", "Bootstrap", "Charts.js", "Axios"],
+                    repo: "https://github.com/kqarlos/election-year",
                     deployed: "https://electionyear2020.herokuapp.com/"
                 });
                 break;
@@ -25,8 +37,9 @@ module.exports = function (app) {
                     projectDisplay: 'block',
                     aboutDisplay: 'none',
                     name: "Memory Game",
-                    description: "",
-                    repo: "",
+                    description: "Choose a game theme and start playing. Only click each image once. Click twice and it's game over",
+                    stack: ["React", "Node", "Bootstrap", "Axios"],
+                    repo: "https://github.com/kqarlos/memory-game",
                     deployed: "https://kqarlos.github.io/memory-game/"
                 });
                 break;
@@ -35,8 +48,9 @@ module.exports = function (app) {
                     projectDisplay: 'block',
                     aboutDisplay: 'none',
                     name: "Note Taker",
-                    description: "",
-                    repo: "",
+                    description: "Real-time note taking application. Take notes, update them, and delete them with this note-taker.",
+                    stack: ["Node", "Express", "Bootstrap"],
+                    repo: "https://github.com/kqarlos/note-taker",
                     deployed: "https://note-taker2020.herokuapp.com/"
                 });
                 break;
@@ -45,9 +59,10 @@ module.exports = function (app) {
                     projectDisplay: 'block',
                     aboutDisplay: 'none',
                     name: "Google Library",
-                    description: "",
-                    repo: "",
-                    deployed: "https://afternoon-fortress-59314.herokuapp.com/"
+                    description: "Search through Google Books API and keep a list of your favorite books.",
+                    stack: ["MongoDB", "Express", "React", "Node", "Bootstrap", "Axios", "Mongoose"],
+                    repo: "https://github.com/kqarlos/google-library",
+                    deployed: "https://google-library-mern.herokuapp.com/"
                 });
                 break;
             case 'passwordGenerator':
